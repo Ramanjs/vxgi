7b3f2faaa151b99810990856826fa3a55ea443e6
Ramanjs <raman.s2662@gmail.com>
2023-11-03 03:56:49 +0530

    use createWindow routine from utils.h to init window

--- src/main.cpp
+++ src/main.cpp
@@ -3,44 +3,15 @@
 
 #include <iostream>
 
-void framebufferSizeCallback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-const char *vertexShaderSource = "#version 440 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
-const char *fragmentShaderSource = "#version 440 core\n"
-    "out vec4 FragColor;\n"
-    "void main()\n"
-    "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-    "}\n\0";
-
 int main(){
 
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Learn OpenGL", NULL, NULL);
-    if (window == NULL) {
-        std::cout<<"Failed to create GLFW window"<<std::endl;
-        glfwTerminate();
-        return -1;
-    }
-
-    glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
-
-    glewInit();
+    GLFWwindow* window = setupWindow(SCR_WIDTH, SCR_HEIGHT);
 
     // shaders
     Shader shader = Shader("./shaders/vs.vert", "./shaders/fs.frag");
@@ -99,10 +70,6 @@ int main(){
 
     glfwTerminate();
     return 0;
-}
-
-void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
-    glViewport(0, 0, width, height);
 }
 
 void processInput(GLFWwindow* window) {

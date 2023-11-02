66569b4e0ad3f5e2700d38b543628df7a9ca57e4
Ramanjs <raman.s2662@gmail.com>
2023-11-03 03:56:25 +0530

    utility functions to initialize opengl

---
+++ src/utils.cpp
@@ -0,0 +1,69 @@
+#include "utils.h"
+
+int openGLInit(){
+    bool err = glewInit() != GLEW_OK;
+        
+    if (err) {
+        fprintf(stderr, "Failed to initialize OpenGL loader!\n");
+        exit(1);
+    }
+
+    //Enable multisampling
+    glEnable(GL_MULTISAMPLE);
+
+    // Enable Blending
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    // Set point size
+    glPointSize(10.0f);
+
+    // Enable smooth point rendering
+    glEnable(GL_LINE_SMOOTH);
+    glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
+
+    return 0;
+}
+
+static void glfwErrorCallback(int error, const char* description)
+{
+    fprintf(stderr, "Glfw Error %d: %s\n", error, description);
+}
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+GLFWwindow* setupWindow(int width, int height)
+{
+    // Setup window
+    glfwSetErrorCallback(glfwErrorCallback);
+    if (!glfwInit())
+        exit(1);
+
+    // Create window with graphics context
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(width, height, "VXGI", NULL, NULL);
+    if (window == NULL) {
+        std::cout<<"Failed to create GLFW window"<<std::endl;
+        glfwTerminate();
+        exit(1);
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+    glfwSwapInterval(1); // Enable vsync
+
+    // Initialize OpenGL loader
+    int status = openGLInit();
+    if(!status){
+        std::cout << "Initialized OpenGL succesfully " << std::endl;
+    }
+    std::cout<< "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
+
+    return window;
+}
--- src/utils.h
+++ src/utils.h
@@ -1,2 +1,11 @@
+#ifndef UTILS_H
+#define UTILS_H
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
+
+GLFWwindow* setupWindow(int width, int height);
+
+#endif /* ifndef UTILS_H */

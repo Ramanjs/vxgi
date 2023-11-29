5dbc4cdaf3297c905b8f6f669330921c61a6413a
Ramanjs <raman.s2662@gmail.com>
2023-11-29 19:48:17 +0530

    include and init Dear ImGui

--- src/utils.h
+++ src/utils.h
@@ -1,6 +1,9 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>

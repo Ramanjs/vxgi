cabe890a0a0e9a6fce2f2c58a80adf025a30bba6
Ramanjs <raman.s2662@gmail.com>
2023-11-29 21:00:50 +0530

    use defined constant for window resolution

--- src/editor.cpp
+++ src/editor.cpp
@@ -37,11 +37,11 @@ void Editor::renderEditor() {
   ImGui_ImplOpenGL3_NewFrame();
   ImGui_ImplGlfw_NewFrame();
   ImGui::NewFrame();
-  ImGui::SetNextWindowSize(ImVec2(400, 720));
+  ImGui::SetNextWindowSize(ImVec2(EDITOR_WIDTH, EDITOR_HEIGHT));
   ImGui::SetNextWindowPos(ImVec2(0, 0));
   ImGui::Begin("VXGI Editor");
-  ImGui::Text("Move Point Light");
-  ImGui::SliderFloat3("World Position", glm::value_ptr(lightPosition), -100.0f,
+  ImGui::Text("Point Light");
+  ImGui::SliderFloat3("Position", glm::value_ptr(lightPosition), -100.0f,
                       2000.0f);
   if (ImGui::IsItemEdited()) {
     revoxelize = true;

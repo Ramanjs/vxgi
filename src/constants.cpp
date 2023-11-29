6564322d938ee7d736c6fa89dab46e4359e4aaa4
Ramanjs <raman.s2662@gmail.com>
2023-11-29 20:59:46 +0530

    add editor resolution constants

--- src/constants.cpp
+++ src/constants.cpp
@@ -3,3 +3,7 @@
 const int VOXEL_DIM = 512;
 const unsigned int SCR_WIDTH = 1280;
 const unsigned int SCR_HEIGHT = 720;
+const unsigned int EDITOR_WIDTH = 300;
+const unsigned int EDITOR_HEIGHT = SCR_HEIGHT;
+const unsigned int VIEWPORT_WIDTH = SCR_WIDTH - EDITOR_WIDTH;
+const unsigned int VIEWPORT_HEIGHT = SCR_HEIGHT;
--- src/constants.h
+++ src/constants.h
@@ -4,5 +4,9 @@
 extern const int VOXEL_DIM;
 extern const unsigned int SCR_WIDTH;
 extern const unsigned int SCR_HEIGHT;
+extern const unsigned int EDITOR_WIDTH;
+extern const unsigned int EDITOR_HEIGHT;
+extern const unsigned int VIEWPORT_WIDTH;
+extern const unsigned int VIEWPORT_HEIGHT;
 
 #endif /* ifndef CONSTANT_H */

582b64e06b5a579881f522ae5d7a1ab5ed8302c6
Ramanjs <raman.s2662@gmail.com>
2023-11-29 20:46:57 +0530

    remove glfw input processing

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,7 @@
 
 #include <iostream>
 
-void processInput(GLFWwindow *window);
-void mouseCallback(GLFWwindow *window, double xposIn, double yposIn);
-void scrollCallback(GLFWwindow *window, double xoffset, double yoffset);
-
 Camera camera(glm::vec3(100, 100, 100));
-float lastX = SCR_WIDTH / 2.0f;
-float lastY = SCR_HEIGHT / 2.0f;
-bool firstMouse = true;
-
-// timing
-float deltaTime = 0.0f; // time between current frame and last frame
-float lastFrame = 0.0f;
 
 int main() {
 
@@ -39,21 +28,15 @@ int main() {
       "shaders/voxelization.geom", "shaders/vis.vert", "shaders/vis.frag",
       "shaders/vct.vert", "shaders/vct.frag", scene, shadowMap);
 
-  glfwSetCursorPosCallback(window, mouseCallback);
-  glfwSetScrollCallback(window, scrollCallback);
+  glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
   Editor editor = Editor(scene, shadowMap, voxelMap, camera);
 
   while (!glfwWindowShouldClose(window)) {
-    float currentFrame = static_cast<float>(glfwGetTime());
-    deltaTime = currentFrame - lastFrame;
-    lastFrame = currentFrame;
-
-    processInput(window);
-
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+    editor.processCameraInput(window);
     editor.renderEditor();
     editor.renderScene();
 
@@ -63,45 +46,4 @@ int main() {
 
   glfwTerminate();
   return 0;
-}
-
-void processInput(GLFWwindow *window) {
-  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-    glfwSetWindowShouldClose(window, true);
-
-  if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-    camera.processKeyboard(Camera_Movement::FORWARD, deltaTime);
-  if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-    camera.processKeyboard(Camera_Movement::BACKWARD, deltaTime);
-  if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-    camera.processKeyboard(Camera_Movement::LEFT, deltaTime);
-  if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-    camera.processKeyboard(Camera_Movement::RIGHT, deltaTime);
-  if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
-    camera.processKeyboard(Camera_Movement::UP, deltaTime);
-  if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
-    camera.processKeyboard(Camera_Movement::DOWN, deltaTime);
-}
-
-void mouseCallback(GLFWwindow *window, double xposIn, double yposIn) {
-  float xpos = static_cast<float>(xposIn);
-  float ypos = static_cast<float>(yposIn);
-
-  if (firstMouse) {
-    lastX = xpos;
-    lastY = ypos;
-    firstMouse = false;
-  }
-
-  float xoffset = xpos - lastX;
-  float yoffset = lastY - ypos;
-
-  lastX = xpos;
-  lastY = ypos;
-
-  camera.processMouseMovement(xoffset, yoffset);
-}
-
-void scrollCallback(GLFWwindow *window, double xoffset, double yoffset) {
-  camera.processMouseScroll(static_cast<float>(yoffset));
 }
